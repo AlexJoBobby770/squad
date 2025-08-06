9cb9616a35fba781087c487672aa6199744f5033
alex jo bobby <alexjocoding@gmail.com>
2025-08-06 22:35:27 +0530

    mongo connection

--- backend/server.js
+++ backend/server.js
@@ -8,14 +8,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// --- Connect to MongoDB ---
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log("MongoDB connected"))
+
+mongoose.connect(process.env.MONGO_URI, ).then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-// --- Sample route ---
+
 app.get("/", (req, res) => res.send("SquadBoard Backend Running ✅"));
 
 const PORT = process.env.PORT || 5000;
