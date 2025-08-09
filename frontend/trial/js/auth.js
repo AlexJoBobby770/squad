31bf7bc3ab279979b3ed9a0f61390d3cc571ddd2
alex jo bobby <alexjocoding@gmail.com>
2025-08-09 13:45:39 +0530

    auth

---
+++ frontend/trial/js/auth.js
@@ -0,0 +1,24 @@
+document.getElementById("loginForm").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const res = await fetch("http://localhost:5000/api/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, password })
+  });
+
+  const data = await res.json();
+
+  if (data.token) {
+  
+    localStorage.setItem("token", data.token);
+    
+    window.location.href = "wall.html";
+  } else {
+  
+    alert(data.message || "Login failed");
+  }
+});
