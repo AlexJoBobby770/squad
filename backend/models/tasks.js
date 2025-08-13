4a0fa35bf63607adc489f16bbbcdafdcf59a40f9
alex jo bobby <alexjocoding@gmail.com>
2025-08-13 22:44:08 +0530

    .

---
+++ backend/models/tasks.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  completed: { type: Boolean, default: false },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Task", taskSchema);
